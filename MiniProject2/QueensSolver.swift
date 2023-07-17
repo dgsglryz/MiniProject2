7229bdd05935b97b6d2f85669b3bc8c41b5b8a81
dgsglryz <dogusguleryuz@gmail.com>
2023-07-17 03:32:29 -0700

    project

---
+++ MiniProject2/QueensSolver.swift
@@ -0,0 +1,14 @@
+//
+//  QueensSolver.swift
+//  SwiftAGDS
+//
+//  Created by Derrick Park on 2019-03-13.
+//  Copyright © 2019 Derrick Park. All rights reserved.
+//
+
+import Foundation
+
+
+
+
+
--- MiniProject2/main.swift
+++ MiniProject2/main.swift
@@ -1,11 +1,33 @@
-//
-//  main.swift
-//  MiniProject2
-//
-//  Created by Helin Akın on 2023-07-17.
-//
-
 import Foundation
 
-print("Hello, World!")
+func solveQueens(board: inout Board) {
+    var count = 0
+    
+    func backtrack(col: Int) -> Bool {
+        count += 1
+        
+        if col >= board.size {
+            print(board)
+            return true
+        }
+        
+        for row in 0..<board.size {
+            if board.isSafe(row: row, col: col) {
+                board.place(row: row, col: col)
+                if backtrack(col: col + 1) {
+                    return true
+                }
+                board.remove(row: row, col: col)
+            }
+        }
+        
+        return false
+    }
+    
+    backtrack(col: 0)
+    
+    print("Number of recursive calls:", count)
+}
 
+var board = Board(size: 8)
+solveQueens(board: &board)
